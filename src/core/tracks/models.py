32accbf639de1554474238580de73954adcaf292
AltairGeo <altairgeo000@gmail.com>
2026-04-18 02:00:30 +0300

    Create base data models for track and track_embedding

---
+++ src/core/tracks/models.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass(slots=True)
+class Track:
+    id: str
+    title: str
+    artist: str
+    genre: str
+    year: int
+    album: str
+    additional_info: str
+    license: str
+
+@dataclass(slots=True)
+class TrackEmbedding:
+    track_id: str
+    vector: list[float]
