f0e3a2622466c5bb0596e27ebd634085d82751a1
AltairGeo <altairgeo000@gmail.com>
2026-04-18 02:49:37 +0300

    Create models for database

---
+++ src/driven/database/tracks/models.py
@@ -0,0 +1,42 @@
+from src.driven.database.session import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ARRAY, FLOAT, ForeignKey, String
+from src.core.tracks.models import Track
+
+
+class TrackModel(BaseModel):
+    __tablename__ = "tracks"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    artist: Mapped[str] = mapped_column(String(200), nullable=False)
+    genre: Mapped[str] = mapped_column(String(100), nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    album: Mapped[str] = mapped_column(String(150), nullable=False)
+    additional_info: Mapped[str]
+    license: Mapped[str]
+    embedding_id: Mapped[int] = mapped_column(ForeignKey("tracks_embeddings.id"), nullable=True)
+    embedding: Mapped["TrackEmbeddingModel"] = relationship(lazy="joined")
+
+    @property
+    def audio_url(self) -> str:
+        return f"/api/tracks/{self.id}/audio"
+
+    def dump_to_domain(self) -> Track:
+        return Track(
+            additional_info=self.additional_info,
+            album=self.album,
+            artist=self.artist,
+            audio_url=self.audio_url,
+            genre=self.genre,
+            id=self.id,
+            license=self.license,
+            title=self.title,
+            year=self.year
+        )
+
+class TrackEmbeddingModel(BaseModel):
+    __tablename__ = "tracks_embeddings"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    vector: Mapped[list[float]] = mapped_column(ARRAY(FLOAT), nullable=False)
