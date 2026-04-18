046fcea923694957b8faf95de517c71c745be493
AltairGeo <altairgeo000@gmail.com>
2026-04-19 02:41:52 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,13 +7,14 @@ load_dotenv()
 
 class DataBaseConfig:
     def __init__(self) -> None:
-        self.dsn_url: str = os.getenv("DATABASE_DSN", "sqlite+aiosqlite:///blog.db")
+        self.dsn_url: str = os.getenv("DATABASE_DSN", "sqlite+aiosqlite:///app.db")
 
 
 class MainConfig:
     def __init__(self, db_config: DataBaseConfig = DataBaseConfig()) -> None:
         self.db = db_config
         self.per_page: int = 25
+        self.music_data_folder: Path = Path("mus_storage")
 
 
 app_config = MainConfig()
