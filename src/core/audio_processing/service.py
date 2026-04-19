a720a7ceb672d1d07141fdd6579e188c08cb55b3
AltairGeo <altairgeo000@gmail.com>
2026-04-19 04:41:02 +0300

    Create service.py

---
+++ src/core/audio_processing/service.py
@@ -0,0 +1,20 @@
+from typing import BinaryIO
+from .processor import AudioProcessor
+from src.core.tracks.domains import Track, TrackEmbedding
+from src.driven.database.tracks.dao import EmbeddingsCrudDAO
+
+
+class AudioEmbeddingService:
+    def __init__(self, audio_processor: AudioProcessor, emb_dao: EmbeddingsCrudDAO) -> None:
+        self.emb_dao = emb_dao
+        self._audio_proccessor = audio_processor
+
+    async def create_embedding(self, file: BinaryIO) -> TrackEmbedding:
+        embedding = self._audio_proccessor.create_embedding(file)
+        vector = embedding.tolist()
+        embedding_id = await self.emb_dao.create(vector=vector)
+
+        if not embedding_id:
+            raise Exception("Embedding: fail to create in database! file: %s", BinaryIO)
+
+        return TrackEmbedding(id=embedding_id, vector=vector)
