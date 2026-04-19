aa3b89b12b7b2f3ac5db1bda699b60cb28a378de
AltairGeo <altairgeo000@gmail.com>
2026-04-19 18:26:58 +0300

    Implement TracksCrudService

--- src/core/tracks/services.py
+++ src/core/tracks/services.py
@@ -1,9 +1,17 @@
-from pathlib import Path
 from .ports import TracksCrudPort
+from typing import Sequence
+from src.core.tracks.domains import Track
 
 
 class TracksCrudService:
     def __init__(self, tracks_crud_port: TracksCrudPort) -> None:
-        self.tracks_crud = tracks_crud_port
+        self._tracks = tracks_crud_port
 
-    async def add_track_and_create_embedding(self, path_to_file: Path) -> bool: ...
+    async def get_track(self, track_id: int) -> Track | None:
+        return await self._tracks.get(track_id)
+
+    async def list_tracks(self, skip: int = 0, limit: int = 25):
+        return await self._tracks.read(skip=skip, limit=limit)
+
+    async def find_by_name(self, name: str) -> Sequence[Track]:
+        return await self._tracks.find_by_name(name)
