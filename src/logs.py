f0950728cc51814bf1ea49d13aa3acfc30c472fb
AltairGeo <altairgeo000@gmail.com>
2026-04-22 01:02:51 +0300

    Create logs.py

---
+++ src/logs.py
@@ -0,0 +1,16 @@
+import logging
+
+_log_format = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
+
+def set_logs() -> None:
+    logging.basicConfig(
+        format=_log_format,
+        level=logging.INFO,
+        filename="habroscrapper.log",
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter(_log_format))
+    logging.getLogger("").addHandler(console)
